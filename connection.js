f04822148de494e9c00a0cca852f8ca9fc4fc3f2
EA - Matheus Henrique Lopes Rodrigues <matheus_lopes01@educadventista.org>
2024-04-17 22:13:16 -0300

    tratando dado

--- connection.js
+++ connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://" + process.env.USER + ":" + process.env.CLUSTER + "/" + process.env.DB + "?retryWrites=true&w=majority&appName=Cluster0";
+var CLUSTER = encodeURIComponent("CLUSTER");
+
+const uri = "mongodb+srv://" + process.env.USER + ":" + CLUSTER + "/" + process.env.DB + "?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(uri)
     .then(() => console.log('Connected to MongoDB'))
