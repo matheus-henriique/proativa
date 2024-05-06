3335c255684ac54c3c34064a02f6988f8273516e
Luiz223 <iquesilva@outlook.com>
2024-05-06 13:04:43 -0300

    implementação do JWT

--- index.js
+++ index.js
@@ -2,19 +2,16 @@ const express = require('express');
 const app = express();
 
 require('dotenv').config();
-require("./connection.js");
+require('./connection.js');
+const PORT = process.env.PORT || 3000;
+
+const authRouter = require('./src/routes/auth.router');
 
 app.use(express.json());
+app.use('/api/auth', authRouter);
 
-const PORT = process.env.PORT || 3000;
-const user = require('./src/routes/user.router.js');
-const auth = require('./src/routes/auth.router.js');
-
-app.get("/helloworld", (req, res)=>{
-    res.send("Hello World!!");
+app.get('/api/helloworld', (req, res) => {
+  res.send('Hello World!!');
 });
-
-app.use(auth);
-app.use(user);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
