8fc483d45e141174e909e9c4f61086db01e810b6
Matheus Rodrigues <mh3047449@gmail.com>
2024-06-06 20:15:13 -0300

    implementação do crud de clientes finalizados

---
+++ src/routes/cliente.router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const clientesController = require('../controller/Cliente.controller');
+const auth = require('../controller/Auth.controller');
+
+router.post('/cliente', auth.authenticateToken, clientesController.createCliente);
+router.get('/cliente', auth.authenticateToken, clientesController.readCliente);
+router.put('/cliente/:id', auth.authenticateToken, clientesController.updateCliente);
+router.delete('/cliente/:id', auth.authenticateToken, clientesController.deleteCliente);
+
+module.exports = router;
